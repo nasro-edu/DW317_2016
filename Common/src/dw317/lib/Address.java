c320475e634fe743285fc399a3a41fb9f767a65e
acadev CEO <nasro.edu@gmail.com>
2016-09-07 18:29:09 -0400

    added getter and setters methodsfoa all data members

--- Common/src/dw317/lib/Address.java
+++ Common/src/dw317/lib/Address.java
@@ -31,14 +31,37 @@ public class Address {
 	public String getAddress() {
 	return civicNumber + " " + streetName + ", " + this.cityName +", "+this.zipCodeName ;
 	}
-	
-	public String getCivicNumber() {
-	return civicNumber;
-	}
-	
-	public String getStreetName() {
-	return streetName;
-	}
-	
 
-}
+	public String getCivicNumber() {
+		return civicNumber;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public String getZipCodeName() {
+		return zipCodeName;
+	}
+
+	public void setCivicNumber(String civicNumber) {
+		this.civicNumber = civicNumber;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public void setZipCodeName(String zipCodeName) {
+		this.zipCodeName = zipCodeName;
+	}
+	
+	}
